218c8946d141ada103e8f8e791a9cbaf402ca5b1
Stephen Stewart <ssawjkhackomaha@gmail.com>
2012-04-14 14:20:17 -0500

    Latest update for the new voter registration data. -SS

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,9 +1,25 @@
+//dataSource {
+//    pooled = true
+//    driverClassName = "org.h2.Driver"
+//    username = "sa"
+//    password = ""
+//}
+
 dataSource {
-    pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+	pooled = false
+	dbCreate = "update" // one of 'create', 'create-drop','update'
+	url = "jdbc:postgresql://localhost:5432/hackomaha"
+	driverClassName = "org.postgresql.Driver"
+	username = "hackomaha"
+	password = "Fresh123!"
+
+	// NOTE: both of these dialects have worked for me. But some people
+	// recommend using the net.sf version and not the org.hibernate version.
+	// dialect = org.hibernate.dialect.PostgreSQLDialect // honestly, not sure what
+	dialect = net.sf.hibernate.dialect.PostgreSQLDialect // the difference is.
+
 }
+
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = true
@@ -13,20 +29,23 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE"
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+            //url = "jdbc:h2:devDb;MVCC=TRUE"
+			url="jdbc:postgresql://localhost:5432/hackomaha"
+			username = "hackomaha"
+			password = "Fresh123!"
         }
     }
     test {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE"
+            //url = "jdbc:h2:mem:testDb;MVCC=TRUE"
         }
     }
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE"
+            //url = "jdbc:h2:prodDb;MVCC=TRUE"
             pooled = true
             properties {
                maxActive = -1
